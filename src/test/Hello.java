16b22d116903247e3bc9a52db1a931a91293ed0d
ziamiakhel <zmiakhel@gmail.com>
2018-03-04 16:56:20 -0500

    added line 6 code

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -2,6 +2,8 @@ package test;
 
 public class Hello {
 public static void main(String[] args) {
-	System.out.println("Hello Class");
+	System.out.println("Hello Class!");
+	System.out.println("It is almost 5pm!");
+
 }
 }
